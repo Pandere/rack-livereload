6bd0dcf3a9557a870e816efaef67101a5ec6d6f0
Jim Neath <jimneath@googlemail.com>
2016-08-03 10:57:32 +0100

    Hardcoded the ngrok host because I'm lazy as shit

--- lib/rack/livereload/body_processor.rb
+++ lib/rack/livereload/body_processor.rb
@@ -14,7 +14,7 @@ module Rack
       end
 
       def livereload_local_uri
-        "#{protocol}://localhost:#{@options[:live_reload_port]}/livereload.js"
+        "#{protocol}://livereload.ngrok.io/livereload.js"
       end
 
       def initialize(body, options)
